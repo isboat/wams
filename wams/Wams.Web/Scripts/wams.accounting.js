e17b2b87fbc9c28838d7d4b2471c782c9706ee32
isboat <isboat@gmail.com>
2016-04-11 22:41:42 +0100

    added chart.min.js WIP

--- wams/Wams.Web/Scripts/wams.accounting.js
+++ wams/Wams.Web/Scripts/wams.accounting.js
@@ -6,12 +6,20 @@
                 success: function(data) {
                     console.log(data);
                     $("#totalAmount").html("&#x20b5; " + data.TotalDuesAmount + " ghc");
+
+
+                    // Get context with jQuery - using jQuery's .get() method.
+                    var ctx = $("#duesChart").get(0).getContext("2d");
+                    // This will get the first returned node in the jQuery collection.
+                    var myNewChart = new Chart(ctx);
                 },
                 error: function(d) {
                     
                 }
             });
         }
+
+
     }
 })(wams);
 
